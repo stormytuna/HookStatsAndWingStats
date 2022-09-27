dfeb89dc9f744f69f1034f8ae54722015c09541f
stormytuna <stormytuna@outlook.com>
2022-09-27 10:40:30 +0100

    Add wing stats and relevant configs

--- Common/Configs/HookStatsAndWingStatsConfig.cs
+++ Common/Configs/HookStatsAndWingStatsConfig.cs
@@ -39,5 +39,51 @@ namespace HookStatsAndWingStats.Common.Configs
         [Label("Display latching type")]
         [DefaultValue(true)]
         public bool DisplayHookLatchingType { get; set; }
+
+        [Header("Wing Stats")]
+
+        [Label("Display wing stats")]
+        [DefaultValue(true)]
+        public bool DisplayWingStats { get; set; }
+
+        [Label("Wing stats title color")]
+        [DefaultValue(typeof(Color), "255, 0, 127, 255")]
+        public Color WingStatsTitleColor { get; set; }
+
+        [Label("Display current flight time")]
+        [DefaultValue(true)]
+        public bool DisplayCurrentWingTime { get; set; }
+
+        [Label("Display max flight time")]
+        [DefaultValue(true)]
+        public bool DisplayMaxWingTime { get; set; }
+
+        [Label("Combine current and max flight time")]
+        [Tooltip("Will be displayed as Flight time: [current]/[max]")]
+        [DefaultValue(true)]
+        public bool CombineCurrentAndMaxWingTime { get; set; }
+
+        [Label("Display flight time in seconds")]
+        [Tooltip("Disabling this displays flight time in frames")]
+        [DefaultValue(true)]
+        public bool DisplayFlightTimeInSeconds { get; set; }
+
+        [Label("Display wing horizontal speed")]
+        [DefaultValue(true)]
+        public bool DisplayWingHorizontalSpeed { get; set; }
+
+        [Label("Wing horizontal speed is measured in mph")]
+        [Tooltip("Turning this off will display speed in raw units instead of mph\nDisplaying in mph may cause descrepencies due to rounding errors")]
+        [DefaultValue(true)]
+        public bool HorizontalSpeedMeasuredInMPH { get; set; }
+
+        [Label("Display wing vertical speed multiplier")]
+        [DefaultValue(true)]
+        public bool DisplayWingVerticalSpeedMult { get; set; }
+
+        [Label("Display wing vertical speed multiplier even when unknown")]
+        [Tooltip("Sometimes the vertical speed multiplier stat will not be found correctly. With this set to true, it will display as unknown instead of not being displayed")]
+        [DefaultValue(false)]
+        public bool DisplayWingVerticalSpeedMultEvenWhenUnknown { get; set; }
     }
 }
--- Common/GlobalItems/HookStatsAndWingStatsGlobalItem.cs
+++ Common/GlobalItems/HookStatsAndWingStatsGlobalItem.cs
@@ -1,10 +1,11 @@
-using Terraria;
+﻿using Terraria;
 using Terraria.ModLoader;
 using Terraria.ID;
 using System.Collections.Generic;
 using System.Linq;
 using System;
 using HookStatsAndWingStats.Common.Configs;
+using Terraria.DataStructures;
 
 namespace HookStatsAndWingStats.Common.GlobalItems
 {
@@ -15,14 +16,17 @@ namespace HookStatsAndWingStats.Common.GlobalItems
             var mod = ModContent.GetInstance<HookStatsAndWingStats>();
             var modConfig = ModContent.GetInstance<HookStatsAndWingStatsConfig>();
             List<TooltipLine> lines = new List<TooltipLine>();
+            string modName = "Terraria";
+            if (item.ModItem != null)
+                modName = item.ModItem.Mod.Name;
+            string itemName = item.Name;
 
             // Hooks
             // Have to be done manually, vanilla ranges and hooks are hard coded
-            if (mod.vanillaHookStats.ContainsKey(item.type) && modConfig.DisplayHookStats)
+            if ((mod.vanillaHookStats.ContainsKey(item.type) || mod.moddedHookStats.ContainsKey(new(modName, itemName))) && modConfig.DisplayHookStats)
             {
                 var value = mod.vanillaHookStats[item.type];
                 
-                lines.Add(new TooltipLine(Mod, "HookBuffer", ""));
                 TooltipLine temp = new TooltipLine(Mod, "HookTitle", "\n~ HOOK STATS ~");
                 temp.OverrideColor = modConfig.HookStatsTitleColor;
                 lines.Add(temp);
@@ -52,6 +56,75 @@ namespace HookStatsAndWingStats.Common.GlobalItems
             }
 
             // Wings
+            // Can be done mostly through WingStats, vertical speed multiplier is hard coded so need a dict for that
+            if (item.wingSlot > 0 && modConfig.DisplayWingStats)
+            {
+                // Declaring stuff
+                Player player = Main.LocalPlayer;
+                WingStats wingStats = player.GetWingStats(item.wingSlot);
+                bool isEquipped = false;
+                
+                // Check if this item is equipped
+                for (int i = 0; i < player.armor.Length; i++)
+                {
+                    if (player.armor[i].type == item.type && Main.mouseX > Main.screenWidth / 2)
+                        isEquipped = true;
+                }
+
+                // Add title
+                TooltipLine temp = new TooltipLine(Mod, "WingTitle", "\n~ WING STATS ~");
+                temp.OverrideColor = modConfig.WingStatsTitleColor;
+                lines.Add(temp);
+
+                // Flight time
+                // If we're using combined wing times and is equipped - display as combined using players wing time
+                if (modConfig.CombineCurrentAndMaxWingTime && modConfig.DisplayCurrentWingTime && modConfig.DisplayMaxWingTime && isEquipped)
+                {
+                    lines.Add(new TooltipLine(Mod, "WingStat", $"Flight time: {GetWingTime(player.wingTime, true)} / {GetWingTime(player.wingTimeMax, true)}"));
+                }
+                // If we're not using combined and is equipped - display separerately using players wing time
+                else if (isEquipped)
+                {
+                    if (modConfig.DisplayMaxWingTime)
+                        lines.Add(new TooltipLine(Mod, "WingStat", $"Max flight time: {GetWingTime(player.wingTimeMax, true)}"));
+                    if (modConfig.DisplayCurrentWingTime)
+                        lines.Add(new TooltipLine(Mod, "WingStat", $"Current flight time: {GetWingTime(player.wingTime, true)}"));
+                }
+                // If it's not equipped - display only MaxWingTime using items wing time
+                else
+                {
+                    if (modConfig.DisplayMaxWingTime)
+                        lines.Add(new TooltipLine(Mod, "WingStat", $"Max flight time: {GetWingTime(wingStats.FlyTime, false)}"));
+                }
+                // Other stats
+                if (modConfig.DisplayWingHorizontalSpeed && modConfig.HorizontalSpeedMeasuredInMPH)
+                    lines.Add(new TooltipLine(Mod, "WingStat", $"Horizontal speed: {MathF.Round(wingStats.AccRunSpeedOverride * 5.084949379f)}mph"));
+                else if (modConfig.DisplayWingHorizontalSpeed && !modConfig.HorizontalSpeedMeasuredInMPH)
+                    lines.Add(new TooltipLine(Mod, "WingStat", $"Horizontal speed: {MathF.Round(wingStats.AccRunSpeedOverride * 5.084949379f)} units"));
+                if (modConfig.DisplayWingVerticalSpeedMult && (mod.vanillaWingVerticalMults.ContainsKey(item.type) || mod.moddedWingVerticalMults.ContainsKey(new(modName, itemName))))
+                {
+                    // If modded
+                    if (modName != "Terraria" && ModLoader.TryGetMod(modName, out Mod mod1))
+                        lines.Add(new TooltipLine(Mod, "WingStat", $"Verical speed multiplier: {mod.moddedWingVerticalMults[new(modName, itemName)]}%"));
+                    // If vanilla
+                    else
+                        lines.Add(new TooltipLine(Mod, "WingStat", $"Verical speed multiplier: {mod.vanillaWingVerticalMults[item.type]}%"));
+                }
+                else if (modConfig.DisplayWingVerticalSpeedMultEvenWhenUnknown)
+                    lines.Add(new TooltipLine(Mod, "WingStat", $"Verical speed multiplier: Unknown"));
+
+                tooltips.AddRange(lines);
+            }
+        }
+
+        private string GetWingTime(float wingTime, bool isEquipped)
+        {
+            if (Main.LocalPlayer.empressBrooch && isEquipped)
+                return "∞";
+            if (ModContent.GetInstance<HookStatsAndWingStatsConfig>().DisplayFlightTimeInSeconds)
+                return (wingTime / 60f).ToString("0.00") + "s";
+            return
+                wingTime.ToString();
         }
     }
 }
