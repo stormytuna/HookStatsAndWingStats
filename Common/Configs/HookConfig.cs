d6dc35a33133bb4d7d18879aae9fefa90029d4f5
stormytuna <stormytuna@outlook.com>
2023-06-09 20:11:00 +0100

    Backporting

--- Common/Configs/HookConfig.cs
+++ Common/Configs/HookConfig.cs
@@ -7,40 +7,64 @@ namespace HookStatsAndWingStats.Common.Configs;
 
 public class HookConfig : ModConfig
 {
+	private const string configPath = "Mods.HookStatsAndWingStats.Configs.HookConfig";
+
 	public static HookConfig Instance => ModContent.GetInstance<HookConfig>();
 
 	public override ConfigScope Mode => ConfigScope.ClientSide;
 
+	[Label($"${configPath}.ShowStats.Label")]
+	[Tooltip($"${configPath}.ShowStats.Tooltip")]
 	[DefaultValue(true)]
 	public bool ShowStats { get; set; }
 
+	[Label($"${configPath}.CompareStats.Label")]
+	[Tooltip($"${configPath}.CompareStats.Tooltip")]
 	[DefaultValue(true)]
 	public bool CompareStats { get; set; }
 
+	[Label($"${configPath}.DockStats.Label")]
+	[Tooltip($"${configPath}.DockStats.Tooltip")]
 	[DefaultValue(false)]
 	public bool DockStats { get; set; }
 
+	[Label($"${configPath}.ShowTitle.Label")]
+	[Tooltip($"${configPath}.ShowTitle.Tooltip")]
 	[DefaultValue(false)]
 	public bool ShowTitle { get; set; }
 
+	[Label($"${configPath}.TitleColor.Label")]
+	[Tooltip($"${configPath}.TitleColor.Tooltip")]
 	[DefaultValue(typeof(Color), "224, 89, 22, 255")]
 	public Color TitleColor { get; set; }
 
+	[Label($"${configPath}.ShowReach.Label")]
+	[Tooltip($"${configPath}.ShowReach.Tooltip")]
 	[DefaultValue(true)]
 	public bool ShowReach { get; set; }
 
+	[Label($"${configPath}.ReachInTiles.Label")]
+	[Tooltip($"${configPath}.ReachInTiles.Tooltip")]
 	[DefaultValue(true)]
 	public bool ReachInTiles { get; set; }
 
+	[Label($"${configPath}.ShowVelocity.Label")]
+	[Tooltip($"${configPath}.ShowVelocity.Tooltip")]
 	[DefaultValue(true)]
 	public bool ShowVelocity { get; set; }
 
+	[Label($"${configPath}.VelocityInMph.Label")]
+	[Tooltip($"${configPath}.VelocityInMph.Tooltip")]
 	[DefaultValue(false)]
 	public bool VelocityInMph { get; set; }
 
+	[Label($"${configPath}.ShowCount.Label")]
+	[Tooltip($"${configPath}.ShowCount.Tooltip")]
 	[DefaultValue(true)]
 	public bool ShowCount { get; set; }
 
+	[Label($"${configPath}.ShowLatchingType.Label")]
+	[Tooltip($"${configPath}.ShowLatchingType.Tooltip")]
 	[DefaultValue(true)]
 	public bool ShowLatchingType { get; set; }
 }
--- Common/Configs/MiscConfig.cs
+++ Common/Configs/MiscConfig.cs
@@ -7,31 +7,44 @@ namespace HookStatsAndWingStats.Common.Configs;
 
 public class MiscConfig : ModConfig
 {
+	private const string configPath = "Mods.HookStatsAndWingStats.Configs.MiscConfig";
+
 	public static MiscConfig Instance => ModContent.GetInstance<MiscConfig>();
 
 	public override ConfigScope Mode => ConfigScope.ClientSide;
 
+	[Label($"${configPath}.ComparisonTitleColor.Label")]
+	[Tooltip($"${configPath}.ComparisonTitleColor.Tooltip")]
 	[DefaultValue(typeof(Color), "0, 71, 171, 255")]
 	public Color ComparisonTitleColor { get; set; }
 
+	[Label($"${configPath}.ComparisonValueColor.Label")]
+	[Tooltip($"${configPath}.ComparisonValueColor.Tooltip")]
 	[DefaultValue(true)]
 	public bool ComparisonValueColor { get; set; }
 
+	[Label($"${configPath}.ComparisonBetterColor.Label")]
+	[Tooltip($"${configPath}.ComparisonBetterColor.Tooltip")]
 	[DefaultValue(typeof(Color), "0, 255, 0, 255")]
 	public Color ComparisonBetterColor { get; set; }
 
+	[Label($"${configPath}.ComparisonWorseColor.Label")]
+	[Tooltip($"${configPath}.ComparisonWorseColor.Tooltip")]
 	[DefaultValue(typeof(Color), "255, 0, 0, 255")]
 	public Color ComparisonWorseColor { get; set; }
 
+	[Label($"${configPath}.ComparisonEqualColor.Label")]
+	[Tooltip($"${configPath}.ComparisonEqualColor.Tooltip")]
 	[DefaultValue(typeof(Color), "255, 165, 0, 255")]
 	public Color ComparisonEqualColor { get; set; }
 
-	[DefaultValue(true)]
-	public bool ColorBlindAssistanceIcons { get; set; }
-
+	[Label($"${configPath}.StatSubtitleColor.Label")]
+	[Tooltip($"${configPath}.StatSubtitleColor.Tooltip")]
 	[DefaultValue(typeof(Color), "180, 180, 180, 255")]
 	public Color StatSubtitleColor { get; set; }
 
+	[Label($"${configPath}.StatValueColor.Label")]
+	[Tooltip($"${configPath}.StatValueColor.Tooltip")]
 	[DefaultValue(typeof(Color), "255, 255, 255, 255")]
 	public Color StatValueColor { get; set; }
 }
